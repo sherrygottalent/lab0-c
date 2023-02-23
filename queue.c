3b170b8dc17892dfaf9aa2c5c4608c06ebf1e0e7
sherrygottalent <sherrygottalent@Lotnum100>
2023-02-23 08:08:20 +0000

    Implement q_new()

--- queue.c
+++ queue.c
@@ -14,7 +14,9 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
